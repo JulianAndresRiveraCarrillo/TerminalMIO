f1993e20407c1740d7ef9c70f124aa19dfdea484
Julian Andres Rivera Carrillo <julianandresrivera29@hotmail.com>
2020-12-04 23:11:04 -0500

    First GUI added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,29 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	PrincipalController controller;
 	
 	public static void main(String[] args) {
-		System.out.println("PROBLEMA TERMINAL MIO");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxml = new FXMLLoader(getClass().getResource("Principal.fxml"));
+		
+		fxml.setController(controller);
+		
+		Parent root = fxml.load();
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("BIENVENIDO...!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
